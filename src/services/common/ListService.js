ec335c14629f707a2dd689d5dc2f378c9a03a118
Nasrin-Fatema <sarnaqueen@gmail.com>
2024-06-29 17:51:28 +0600

    customerController add

--- src/services/common/ListService.js
+++ src/services/common/ListService.js
@@ -1,8 +1,7 @@
 
-
 const mongoose = require("mongoose");
 
-const ListService = async (Request, DataModel, searchArray, JoinOneStage) => {
+const ListService = async (Request, DataModel, searchArray, JoinOneStage = {}) => {
     try {
         let pageNumber = Number(Request.params.pageNumber) || 1;
         let perPage = Number(Request.params.perPage) || 10;
@@ -11,6 +10,12 @@ const ListService = async (Request, DataModel, searchArray, JoinOneStage) => {
 
         let skip = perPage * (pageNumber - 1);
         let data;
+
+        const basePipeline = [
+            { $match: { userEmail: userEmail } },
+            { $skip: skip },
+            { $limit: perPage }
+        ];
 
         if (searchText !== "null") {
             let query = { $or: searchArray };
@@ -22,7 +27,7 @@ const ListService = async (Request, DataModel, searchArray, JoinOneStage) => {
                 {
                     $facet: {
                         total: [{ $count: "count" }],
-                        data: [{ $skip: skip }, { $limit: perPage }]
+                        data: basePipeline
                     }
                 }
             ]);
@@ -33,7 +38,7 @@ const ListService = async (Request, DataModel, searchArray, JoinOneStage) => {
                 {
                     $facet: {
                         total: [{ $count: "count" }],
-                        data: [{ $skip: skip }, { $limit: perPage }]
+                        data: basePipeline
                     }
                 }
             ]);
@@ -46,3 +51,64 @@ const ListService = async (Request, DataModel, searchArray, JoinOneStage) => {
 };
 
 module.exports = ListService;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require("mongoose");
+
+// const ListService = async (Request, DataModel, searchArray, JoinOneStage) => {
+//     try {
+//         let pageNumber = Number(Request.params.pageNumber) || 1;
+//         let perPage = Number(Request.params.perPage) || 10;
+//         let searchText = Request.params.searchText;
+//         let userEmail = Request.headers.email;
+
+//         let skip = perPage * (pageNumber - 1);
+//         let data;
+
+//         if (searchText !== "null") {
+//             let query = { $or: searchArray };
+
+//             data = await DataModel.aggregate([
+//                 { $match: { userEmail: userEmail } },
+//                 { $match: query },
+//                 JoinOneStage,
+//                 {
+//                     $facet: {
+//                         total: [{ $count: "count" }],
+//                         data: [{ $skip: skip }, { $limit: perPage }]
+//                     }
+//                 }
+//             ]);
+//         } else {
+//             data = await DataModel.aggregate([
+//                 { $match: { userEmail: userEmail } },
+//                 JoinOneStage,
+//                 {
+//                     $facet: {
+//                         total: [{ $count: "count" }],
+//                         data: [{ $skip: skip }, { $limit: perPage }]
+//                     }
+//                 }
+//             ]);
+//         }
+
+//         return { status: "success", data: data[0] }; // Ensure to return the first element which contains 'total' and 'data'
+//     } catch (error) {
+//         return { status: "failed", data: error.message };
+//     }
+// };
+
+// module.exports = ListService;
+
+
